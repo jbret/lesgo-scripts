3dc1aedaf19414e9c7bb6737a29e851602b1888d
Joel Bretheim <jbret@users.noreply.github.com>
2016-03-29 12:27:43 -0400

    Update njord-launch.sh
    minor change to comments

--- njord-launch.sh
+++ njord-launch.sh
@@ -2,7 +2,7 @@
 
 # For launching lesgo jobs on njord cluster.
 # Searches lesgo configuration file for number of processors to be used then launches mpi job.
-# Author: Joel Bretheim, jbretheim@gmail.com
+# Author: Joel Bretheim
 
 conf=lesgo.conf
 
