8938eff1976468ce6083025450d2b3dad34ef2d8
Joel Bretheim <jbretheim@gmail.com>
2016-09-29 18:32:02 -0400

    added capability to rename lesgo binary before launching job

--- njord-launch.sh
+++ njord-launch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # For launching lesgo jobs on njord cluster.
-# Searches lesgo configuration file for number of processors to be used then launches mpi job.
+# Searches lesgo configuration file for number of MPI processes.
+# Renames lesgo binary using a randomly generated integer ID.
+# Launches MPI job.
+#
 # Author: Joel Bretheim
 
 conf=lesgo.conf
@@ -9,7 +12,14 @@ conf=lesgo.conf
 nproc=`grep "nproc" $conf | awk '{print $3}'`
 
 printf "Found nproc = $nproc in $conf \n"
-
 echo $nproc
 
-mpirun -np $nproc ./lesgo-mpi-* > lesgo.out &
+myid=$RANDOM
+range=1000
+let "myid %= $range"
+printf "Assigning job ID: $myid \n"
+caseName="mycase"
+caseName=$caseName$myid
+cp lesgo-mpi-* $caseName
+
+mpirun -np $nproc ./$caseName > lesgo.out &
